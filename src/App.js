ce6162a1161e4be27057b3e9c86e26b80a7db357
pranavv481 <pranavv481@gmail.com>
2020-05-27 10:03:33 +0530

    multiple reducers

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import CounterThree from './components/CounterThree';
+// import CounterOne from './components/CounterOne';
+// import CounterTwo from './components/CounterTwo';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <CounterOne /> */}
+      {/* <CounterTwo /> */}
+      <CounterThree />
+    </div>
+  );
+}
+
+export default App;
